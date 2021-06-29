8855a70d9ee2670156caafd59251f052b4e34454
Darya <darya.aniskevich@mail.ru>
2021-06-29 17:07:55 +0300

    all components added to app

--- movies-app/src/App.js
+++ movies-app/src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import Header from "./components/Header";
+import Results from "./components/Results";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Results />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
