93e09aaba65fb21fc2aedf86b9092eca97a50e80
Darya <darya.aniskevich@mail.ru>
2021-06-29 15:54:53 +0300

    movie card created

---
+++ movies-app/src/components/Results/MovieCard/MovieCard.jsx
@@ -0,0 +1,22 @@
+import style from "./MovieCard.module.css";
+
+const MovieCard = () => {
+  return (
+    <div className={style.card}>
+      <div className={style.imageBlock}>
+        <img src="#" alt="film name" classNme={style.image} />
+      </div>
+      <div className={style.description}>
+        <div className={style.titleBlock}>
+          <div className={style.title}>Title</div>
+          <div className={style.ganre}>Ganre</div>
+        </div>
+        <div className={style.releaseBlock}>
+          <div className={style.release}>date</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
