ef37ccf3e3e51d0d65c13fa5de4e4d2e079a8a68
Darya <darya.aniskevich@mail.ru>
2021-07-02 12:56:02 +0300

    react-redux added

--- movies-app/src/components/Results/Results.jsx
+++ movies-app/src/components/Results/Results.jsx
@@ -1,18 +1,29 @@
 import style from "./Results.module.css";
 import ResultsTopBar from "./ResultsTopBar";
 import MovieCard from "./MovieCard";
+import { useDispatch, useSelector } from "react-redux";
+import { getMoviesData } from "../../store/moviesActions";
+import { useEffect } from "react";
 
 const Results = () => {
+  const dispatch = useDispatch();
+
+  const movies = useSelector((state) => state.movies.movies);
+  const moviesIsLoading = useSelector((state) => state.movies.isLoading);
+
+  useEffect(() => {
+    dispatch(getMoviesData());
+  }, [dispatch]);
+
   return (
     <div className={style.results}>
       <ResultsTopBar />
       <div className={style.container}>
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
+        {moviesIsLoading ? (
+          <div>Loading...</div>
+        ) : (
+          movies.map((movie, i) => <MovieCard key={movie.id} />)
+        )}
       </div>
     </div>
   );
